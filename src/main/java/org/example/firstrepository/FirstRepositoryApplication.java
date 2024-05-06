adf82ceb0911e91080cf5f4ea4902db75c0697c6
Konstantin <sabanovkostan@gmail.com>
2024-05-06 20:54:06 +0600

    first commit

---
+++ src/main/java/org/example/firstrepository/FirstRepositoryApplication.java
@@ -0,0 +1,13 @@
+package org.example.firstrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRepositoryApplication.class, args);
+	}
+
+}
